83988eefa4626e42c2288015bdc8c7147ea8f222
Yoginlin <2829536126@qq.com>
2021-06-03 22:45:08 +0800

    22点45分

--- Vue_Code/02_Learn_Webpack/03_webpack-资源/webpack.config.js
+++ Vue_Code/02_Learn_Webpack/03_webpack-资源/webpack.config.js
@@ -4,8 +4,7 @@ module.exports = {
   entry: "./src/main.js",
   output: {
     path: path.resolve(__dirname, "./build"),
-    filename: "bundle.js",
-    publicPath: '/',
+    filename: "bundle.js"
   },
   module: {
     rules: [
