c4ea80db97f90bfbfb663ec732c96b2ee149d3a9
yoginlin <2829536126@qq.com>
2021-06-04 17:29:10 +0800

    2021年6月4日17:29:09

--- Vue_Code/02_Learn_Webpack/03_webpack-资源/src/js/element.js
+++ Vue_Code/02_Learn_Webpack/03_webpack-资源/src/js/element.js
@@ -2,6 +2,9 @@ import "../css/title.less";
 // 对css进行引用，让其可以打包
 import "../css/image.css";
 
+// 图片也是一个模块，将其导入
+import zznhImage from '../img/zznh.png'
+
 const divEl = document.createElement("div");
 divEl.className = "title";
 divEl.innerHTML = "你好啊，李银河";
@@ -12,7 +15,7 @@ bgDivEl.className = "image-bg";
 
 // 设置img元素的src
 const imgEl = document.createElement("img")
-imgEl.src = "../img/zznh.png";
+imgEl.src = zznhImage;
 
 document.body.appendChild(divEl);
 document.body.appendChild(bgDivEl);
--- Vue_Code/02_Learn_Webpack/03_webpack-资源/webpack.config.js
+++ Vue_Code/02_Learn_Webpack/03_webpack-资源/webpack.config.js
@@ -26,8 +26,20 @@ module.exports = {
       {
         // ?表示0个或者1个
         test:/\.(jpe?g|png|gif|svg)$/,
-        use:"file-loader",
+        // use:"file-loader",
 				// 搞定之后再次打包，build文件下会出现一张图片，名字是一串字符
+
+        // 当我们想给打包好的文件按照一定格式命名时，可以这样写
+        use:{
+          loader:"file-loader",
+          // 可以在options里面写上一些而外的参数
+          options:{
+            // 输出的一个路径
+            outputPath:"img",
+            // name决定图片的一个名字，[]是一种占位写法
+            name:"[name].png"
+          }
+        }
       }
     ],
   },
