ca7b030278f93e67e5fcf4a722021a59b7bfa38e
yoginlin <2829536126@qq.com>
2021-06-25 18:51:13 +0800

    sj

--- Vue_Code/02_Learn_Webpack/10_vite的基本使用/src/main.js
+++ Vue_Code/02_Learn_Webpack/10_vite的基本使用/src/main.js
@@ -1,9 +1,24 @@
-import _ from "../node_modules/lodash-es/lodash.default.js";
-import { sum } from "./js/math";
+// import _ from "../node_modules/lodash-es/lodash.default";
+import _ from "lodash-es"
+import { sum } from "./js/math"
+import mul from "./ts/mul"
 
-console.log("Hello World !!!");
-console.log(sum(20, 30));
+import "./css/style.css"
+import "./css/title.less"
 
-console.log(_.join(["abc", "cba"], "-"));
+console.log("Hello World !!!")
+console.log(sum(20, 30))
+console.log(mul(20, 30))
 
-// 01 31 11
+console.log(_.join(["abc", "cba"], "-"))
+
+const titleEl = document.createElement("div")
+titleEl.className = "title"
+titleEl.innerHTML = "Hello"
+document.body.appendChild(titleEl)
+
+/* 
+  1、用了Vite，.js后缀名可以不加
+  2、可以自动解析路径
+  3、帮我们打包代码
+*/
