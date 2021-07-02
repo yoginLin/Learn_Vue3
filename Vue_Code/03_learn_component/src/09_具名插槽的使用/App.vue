e667ebaaad2e640c3e770fcaa9c66791408863ab
Yoginlin <2829536126@qq.com>
2021-07-02 23:00:30 +0800

    ·1

---
+++ Vue_Code/03_learn_component/src/09_具名插槽的使用/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <nav-bar :name="name">
+      <!-- 具名插槽的好处就是可以指定元素的分布 -->
+      <template v-slot:left>
+        <button>左边的按钮</button>
+      </template>
+
+      <template v-slot:center>
+        <h2>我是中间的标题</h2>
+      </template>
+
+      <template v-slot:right>
+        <i>右边的i元素</i>
+      </template>
+
+      <!-- 注意得这样用 v-slot:[name] -->
+      <template v-slot:[name]>
+        <i>why元素</i>
+      </template>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from "./NavBar.vue";
+export default {
+  components: { NavBar },
+  data() {
+    return {
+      name: "why",
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ Vue_Code/03_learn_component/src/09_具名插槽的使用/NavBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="nav-bar">
+    <!-- <slot></slot> 等同于 <slot name="default"></slot> -->
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <div class="center">
+      <slot name="center"></slot>
+    </div>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+
+    <div class="addition">
+      <slot :name="name"></slot>
+    </div>
+  </div>
+
+  <!-- 具名插槽的好处就是可以指定元素的分布 -->
+</template>
+
+<script>
+export default {
+  props: {
+    name: String
+  },
+  // data() {
+  //   return {
+  //     name: "why"
+  //   };
+  // },
+};
+</script>
+
+<style scoped>
+.nav-bar {
+  display: flex;
+}
+
+.left,
+.right,
+.addition {
+  width: 80px;
+  background-color: pink;
+}
+
+.center {
+  flex: 1;
+  background-color: blue;
+}
+
+.left,
+.center,
+.right {
+  height: 44px;
+}
+</style>
