62cffeceaf855e08a03c623b357893ff5db95c83
yoginlin <2829536126@qq.com>
2021-07-17 18:05:29 +0800

    Mixin的介绍

---
+++ Vue_Code/05_composition_api/src/01_mixin和extends/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+    <button @click="foo">我是按钮</button>
+  </div>
+</template>
+
+<script>
+  import { demoMixin } from './mixins/demoMixin'
+  export default {
+    // mixin本质上就是一个对象
+    mixins: [demoMixin],
+    data() {
+      return {
+        title: 'App Hello',
+        message: 'APP message'
+        // 15:30
+      }
+    },
+    methods: {},
+  }
+</script>
+
+<style scoped></style>
